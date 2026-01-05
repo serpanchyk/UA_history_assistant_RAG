4475ae9871712b04f113d597f6676af556f3d2ff
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2026-01-05 17:11:50 +0200

    fix: divide scrapping on text and images.

--- ingest/scrap_data.py
+++ ingest/scrap_data.py
@@ -2,44 +2,60 @@ import pymupdf
 from pathlib import Path
 import pandas as pd
 
-def scrap_data():
-    docs_path = Path('../data/pdfs')
-    images_path = Path('../data/images')
-    dfs_path = Path('../data/dfs')
-    images_path.mkdir(parents=True, exist_ok=True)
+DOCS_PATH = Path('../data/pdfs')
+IMAGES_PATH = Path('../data/images')
+IMAGES_PATH.mkdir(parents=True, exist_ok=True)
+DFS_PATH = Path('../data/dfs')
 
-    df_docs = pd.read_csv(dfs_path / 'docs.csv', index_col='id')
+def scrap_text():
+    df_docs = pd.read_csv(DFS_PATH / 'docs.csv', index_col='id')
 
     rows_text = []
-    rows_images = []
     for index, doc_row in df_docs.iterrows():
-        file_path = docs_path / doc_row['pdf_name']
+        file_path = DOCS_PATH / doc_row['pdf_name']
         doc = pymupdf.open(file_path)
 
         for page in doc:
             page_data = page.get_text('dict')
             for block in page_data['blocks']:
+                if block['type'] == 1:
+                    continue
+                text = ' '.join(
+                    span['text']
+                    for line in block['lines']
+                    for span in line['spans']
+                )
+
+                rows_text.append({
+                    'text': text,
+                    'bbox': tuple(block['bbox']),
+                    'page': page.number,
+                    'doc_id': index
+                })
+
+
+    df_text = pd.DataFrame(rows_text)
+
+    df_text.to_csv(DFS_PATH / 'texts.csv')
+
+def scrap_images():
+    df_docs = pd.read_csv(DFS_PATH / 'docs.csv', index_col='id')
+
+    rows_images = []
+    for index, doc_row in df_docs.iterrows():
+        file_path = DOCS_PATH / doc_row['pdf_name']
+        doc = pymupdf.open(file_path)
+
+        for page in doc:
+            page_data = page.get_text('dict')
+            for i, block in enumerate(page_data['blocks']):
                 if block['type'] == 0:
-                    text = ' '.join(
-                        span['text']
-                        for line in block['lines']
-                        for span in line['spans']
-                    )
+                    continue
 
-                    rows_text.append({
-                        'text': text,
-                        'bbox': tuple(block['bbox']),
-                        'page': page.number,
-                        'doc_id': index
-                    })
+                ext = block["ext"]
+                img_bytes = block["image"]
 
-            for image in page.get_images(full=True):
-                xref = image[0]
-                d = doc.extract_image(xref)
-                ext = d["ext"]
-                img_bytes = d["image"]
-
-                image_path = images_path / f"doc{index}_page{page.number}_{xref}.{ext}"
+                image_path = IMAGES_PATH / f"doc{index}_page{page.number}_{i}.{ext}"
 
                 with open(image_path, "wb") as f:
                     f.write(img_bytes)
@@ -51,12 +67,10 @@ def scrap_data():
                     'doc_id': index
                 })
 
-    df_text = pd.DataFrame(rows_text)
     df_images = pd.DataFrame(rows_images)
-
-    df_text.to_csv(dfs_path / 'texts.csv')
-    df_images.to_csv(dfs_path / 'images.csv')
+    df_images.to_csv(DFS_PATH / 'images.csv')
 
 
 if __name__ == '__main__':
-    scrap_data()
+    scrap_text()
+    scrap_images()
