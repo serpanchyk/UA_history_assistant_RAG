144c4ad2896cd0324f008fc1bab2be62d9660585
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2026-01-06 20:27:31 +0200

    remove: deprecated mammayLLM logic

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from rag.llm import get_response
-
-def question_llm():
-    with open('rag/prompt.txt', 'r') as f:
-        prompt = f.read()
-    get_response(prompt)
 
 
 if __name__ == '__main__':
-    question_llm()
 
