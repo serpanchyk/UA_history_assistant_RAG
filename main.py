0a0dadb2ab542775cd2c72834857a22e606093a0
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2026-01-25 18:11:53 +0200

    feat: add ui

--- main.py
+++ main.py
@@ -4,18 +4,7 @@ from src.index.storage import QdrantVectorStore
 from src.rag.llm_service import LLMService
 
 if __name__ == "__main__":
+    pass
 
-    embedder = EmbeddingService()
-    storage = QdrantVectorStore(embedder)
-
-    llm_service = LLMService(storage)
-
-    while True:
-        query = input("Enter query: ")
-        if query == 'q':
-            break
-
-        response = llm_service.generate_response(query)
-        print(response)
 
 
